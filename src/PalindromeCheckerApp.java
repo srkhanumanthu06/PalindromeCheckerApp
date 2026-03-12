784b3da6c449fcbfe545437f02c1a02298ad5307
srkhanumanthu06 <210135197+srkhanumanthu06@users.noreply.github.com>
2026-03-12 21:59:30 +0530

    Initial commit

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,10 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // UC1: Application Entry & Welcome Message
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Version: 1.0");
+
+    }
+}
