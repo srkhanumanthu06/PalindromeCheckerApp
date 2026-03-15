874ee74096769c3d2c380be89e9c6cefc158f088
srkhanumanthu06 <210135197+srkhanumanthu06@users.noreply.github.com>
2026-03-15 16:42:01 +0530

    Added UC1 Application Entry & Welcome Message for Hotel Booking App

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,13 @@
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+        // UC1: Application Entry & Welcome Message
+        System.out.println("=====================================");
+        System.out.println(" Welcome to Book My Stay Application ");
+        System.out.println(" Hotel Booking Management System v1.0 ");
+        System.out.println("=====================================");
+
+        System.out.println("System started successfully.");
+
+    }
+}
